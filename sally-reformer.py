ce73db12ab5c7b6b7f4a49434bbe26e3e950da91
whatwant <whatwant@gmail.com>
2017-12-16 21:47:07 +0900

    [#3][#4] make class for file management. and update pattern.csv

--- sally-reformer.py
+++ sally-reformer.py
@@ -19,14 +19,96 @@ INFO = {
     'PATH'           : {},
     'PYTHON_VERSION' : '',
     'PATTERN'        : {},
-    'FILEPATH'       : {},
+    'FILEPATH'       : [],
     'ENCODING'       : ''
     }
     
 
-
-
     
+class TFile:
+    
+    def __init__(self, dirpath, filename):
+        self.dirpath   = dirpath
+        self.filename  = filename
+        self.firstname = filename.split(".")[0]
+        self.lastname  = ".".join( filename.split(".")[1:] )
+    
+
+    def equalsFirstname(self, firstname):
+        return True if( self.firstname == firstname ) else False
+    
+
+    def getFilename(self, decoding=''):
+        result = self.filename
+    
+        if( decoding in ['utf8', 'utf-8'] ):
+            result = self.filename.decode('utf8')
+
+        return result
+
+
+    def getFirstname(self, decoding=''):
+        result = self.firstname
+    
+        if( decoding in ['utf8', 'utf-8'] ):
+            result = self.firstname.decode('utf8')
+
+        return result
+
+
+    def getLastname(self, decoding=''):
+        result = self.lastname
+    
+        if( decoding in ['utf8', 'utf-8'] ):
+            result = self.lastname.decode('utf8')
+
+        return result
+    
+    
+    def getDirpath(self, decoding=''):
+        result = self.dirpath
+    
+        if( decoding in ['utf8', 'utf-8'] ):
+            result = self.dirpath.decode('utf8')
+
+        return result
+    
+    
+    def isExistedFilename(self, dirpath, filename):
+        filenames = os.listdir( dirpath )
+
+        return True if( filename in filenames ) else False
+
+
+    def getNonexistantFilename(self, dirpath, new_filename):
+
+        i = 0
+        while self.isExistedFilename(dirpath, new_filename):
+            filename, extension = os.path.splitext(new_filename)
+
+            i += 1
+            new_filename = "{}-{}{}".format(filename, i, extension)
+    
+        return new_filename
+    
+    
+    def renameFilename(self, new_dirpath, new_filename):
+        old_filepath = os.path.join( self.dirpath, self.filename ).decode( 'utf8' )
+        
+        new_filename = self.getNonexistantFilename( new_dirpath, new_filename )
+        new_filepath = os.path.join( new_dirpath, new_filename ).decode( 'utf8' )
+        
+        os.rename( old_filepath, new_filepath )
+        
+        self.dirpath   = dirpath
+        self.filename  = new_filename
+        self.firstname = new_filename.split(".")[0]
+        
+        return True
+        
+
+
+        
 if __name__ == "__main__":
 
 
@@ -126,106 +208,62 @@ if __name__ == "__main__":
     ##TODO: pattern file rewrite
     
     
-    
-    
-    
+        
     ##### [Load FilePath] #####
     for dirpath, dirnames, filenames in os.walk(INFO['PATH']['source_directory']):
         dirpath = dirpath.decode( INFO['ENCODING'] ).encode('utf8')
         for filename in filenames:
             filename = filename.decode( INFO['ENCODING'] ).encode('utf8')
-            INFO['FILEPATH'][ os.path.join(dirpath, filename) ] = {
-                'DIR'       : dirpath,
-                'FILENAME'  : filename,
-                'FIRSTNAME' : filename.split(".")[0],
-                'LASTNAME'  : ".".join( filename.split(".")[1:] )
-                }
             
+            INFO['FILEPATH'].append( TFile(dirpath, filename) )
+
     print( "[info] load filepath count (%s)" % len(INFO['FILEPATH']) )
-    
-    
+    print
     
     
     
     ##### [Matching Pattern] #####
     RESULT = []
-    for idx, (filepath, items) in enumerate(INFO['FILEPATH'].items()):
+    for idx, tfile in enumerate(INFO['FILEPATH']):
     
-        print items['FILENAME'].decode('utf8'), "(", items['FIRSTNAME'].decode('utf8'), ")", ":",
+        print tfile.getFilename('utf8'), ":",
+        
         for pattern in INFO['PATTERN'].keys():
-            if pattern == items['FIRSTNAME']:
-            
-                ITEM = {
-                    'DIR'  : items['DIR'],
-                    'FROM' : items['FILENAME'],
-                    'TO'   : INFO['PATTERN'][ pattern ] + "." + items['LASTNAME']
-                }
-                print ITEM['TO'].decode( 'utf8' ),
+        
+            if( tfile.equalsFirstname(pattern) ):
+                
+                old_dirpath = tfile.getDirpath('utf8')
+                
+                new_dirpath = INFO['PATH']['source_directory'].decode( 'utf8' )
+                new_filename = INFO['PATTERN'][ pattern ] + "." + tfile.getLastname('utf8')
+                INFO['FILEPATH'][idx].renameFilename( new_dirpath, new_filename )
+                
+                print INFO['FILEPATH'][idx].getFilename('utf8'),
+                
+                if not os.listdir(old_dirpath):
+                    os.rmdir(old_dirpath)
+                
+                break
                 
                 
-                src = os.path.basename( os.path.normpath( ITEM['DIR'].decode( 'utf8' ) ) )
-                dst = ITEM['TO'].decode( 'utf8' )
+            elif INFO['PATTERN'][pattern] == tfile.getFirstname('utf8'):
                 
-                if( src == dst ):
-                    src = ITEM['DIR'].decode( 'utf8' )
-                    dst = ".".join(ITEM['DIR'].split(".")[:-1]).decode('utf8')
-                    
-                    os.rename( src, dst )
-                    ITEM['DIR'] = ".".join(ITEM['DIR'].split(".")[:-1])
+                old_dirpath = tfile.getDirpath('utf8')
                 
-                src = os.path.join( ITEM['DIR'], ITEM['FROM'] ).decode( 'utf8' )
-                dst = os.path.join( INFO['PATH']['source_directory'], ITEM['TO'] ).decode( 'utf8' )
+                new_dirpath = INFO['PATH']['source_directory'].decode( 'utf8' )
+                new_filename = tfile.getFilename('utf8')
+                INFO['FILEPATH'][idx].renameFilename( new_dirpath, new_filename )
                 
-                shutil.move( src, dst)
-                if os.listdir( ITEM['DIR'].decode( 'utf8' ) ) == []:
-                    shutil.rmtree( ITEM['DIR'].decode( 'utf8' ) )
+                print INFO['FILEPATH'][idx].getFilename('utf8'),
                 
-                #print
-                #print os.path.join( ITEM['DIR'], ITEM['FROM'] ).decode( 'utf8' )
-                #print os.path.join( INFO['PATH']['source_directory'], ITEM['TO'] ).decode( 'utf8' )
-                #print
-                
+                if not os.listdir(old_dirpath):
+                    os.rmdir(old_dirpath)
                 
                 break
             
-            
-            elif INFO['PATTERN'][pattern] == items['FIRSTNAME']:
-            
-                ITEM = {
-                    'DIR'  : items['DIR'],
-                    'FROM' : items['FILENAME'],
-                    'TO' : items['FILENAME'],
-                    #'TO'   : INFO['PATTERN'][ pattern ] + "." + items['LASTNAME']
-                }
-                print ITEM['TO'].decode( 'utf8' ),
-                
-                
-                #src = os.path.basename( os.path.normpath( ITEM['DIR'].decode( 'utf8' ) ) )
-                #dst = ITEM['TO'].decode( 'utf8' )
-                
-                #if( src == dst ):
-                #    src = ITEM['DIR'].decode( 'utf8' )
-                #    dst = ".".join(ITEM['DIR'].split(".")[:-1]).decode('utf8')
-                    
-                #    os.rename( src, dst )
-                #    ITEM['DIR'] = ".".join(ITEM['DIR'].split(".")[:-1])
-                
-                src = os.path.join( ITEM['DIR'], ITEM['FROM'] ).decode( 'utf8' )
-                dst = os.path.join( INFO['PATH']['source_directory'], ITEM['TO'] ).decode( 'utf8' )
-                
-                shutil.move( src, dst)
-                if os.listdir( ITEM['DIR'].decode( 'utf8' ) ) == []:
-                    shutil.rmtree( ITEM['DIR'].decode( 'utf8' ) )
-                
-                #print
-                #print os.path.join( ITEM['DIR'], ITEM['FROM'] ).decode( 'utf8' )
-                #print os.path.join( INFO['PATH']['source_directory'], ITEM['TO'] ).decode( 'utf8' )
-                #print
-                
-                
-                break
-    
         print
 
+        
+        
         
     sys.exit()
